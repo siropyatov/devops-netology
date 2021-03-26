c78ae2a3e17b7b018065a0ca7640edf322a99799
i.siropyatov <i.siropyatov@gmail.com>
2021-03-26 14:49:25 +0500

    terraform v4 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
   alias = "region"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "netology-test"
+    region = "us-west-2"
+    key = "terraform.tfstate"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -21,19 +30,32 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  #  instance_type = "t3.micro"
+  instance_type = local.web_instance_type_map[terraform.workspace]
+  count = local.web_instance_count_map[terraform.workspace]
 
   tags = {
     Name = "HelloWorld"
   }
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy = true
+    ignore_changes = [
+      "tags"]
+  }
 }
 
 data "aws_caller_identity" "current" {}
-
-
 data "aws_region" "current" {}
 
 locals {
   account_id    = data.aws_caller_identity.current.account_id
   region        = data.aws_region.current.name
+  web_instance_type_map = {
+    stage = "t3.micro"
+    test = "t3.micro"
+  }
+  web_instance_count_map = {
+    test = 0
+    stage = 1}
 }
